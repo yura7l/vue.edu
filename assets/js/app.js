4934f14eb215642ab84447d3c599fc75feb5a9b6
Yura <yura93lobur@gmail.com>
2021-10-27 23:56:04 +0300

    todo list vue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,62 @@
+const AppThird = {
+    data() {
+        return {
+            appThirdTitle: 'Checklist',
+            appThirdPlaceholder: 'Enter new list item',
+            appThirdEmpty: 'To do list is empty. Please add first item',
+            appThirdValue: '',
+            todoList: [
+                {id: 0, value: 'Lorem ipsum dolor', status: 1, edit: false},
+                {id: 1, value: 'Lorem ipsum dolor', status: 1, edit: false}
+            ]
+        }
+    },
+    methods: {
+        itemCreate(event) {
+            if(this.appThirdValue){
+                this.todoList.push({id: this.todoList.length, value: this.appThirdValue, status: 1})
+                this.appThirdValue = ''
+            }
+        },
+        itemRemove(i, event) {
+            for( let j = 0; j < this.todoList.length; j++){
+                if(this.todoList[j].id === i){
+                    this.todoList.splice(j, 1)
+                    break
+                }
+            }
+        },
+        itemChangeStatus(i, event) {
+            if(this.todoList[i].status === 1){
+                this.todoList[i].status = 2
+            }else{
+                this.todoList[i].status = 1
+            }
+        },
+        itemEdit(i, event) {
+            this.todoList[i].edit = true
+        },
+        itemSave(i, event) {
+            console.log(111)
+            this.todoList[i].edit = false
+        },
+        itemCancel(i, event) {
+            this.todoList[i].edit = false
+        },
+        itemChange(i, event) {
+
+        }
+    },
+    computed: {
+
+    },
+    watch: {
+        appThirdValue(value) {
+
+        }
+    }
+}
+
 const AppFirst = {
     data() {
         return {
@@ -47,6 +106,8 @@ const AppSecond = {
         }
     }
 }
+
+Vue.createApp(AppThird).mount('#app_third')
 
 Vue.createApp(AppFirst).mount('#app_first')
 
