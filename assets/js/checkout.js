ac549c383a53a67294d73e9287cf242e2a62ed27
Yurii <yura93lobur@gmail.com>
2021-11-06 22:35:16 +0200

    checkout promo

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -8,31 +8,28 @@ const AppCheckout = {
                     id: 0,
                     name: 'Design',
                     price: 300,
-                    discount: 0,
                     added: false
                 },
                 {
                     id: 1,
                     name: 'Web development',
                     price: 400,
-                    discount: 0,
                     added: false
                 },
                 {
                     id: 2,
                     name: 'Integration',
                     price: 250,
-                    discount: 0,
                     added: false
                 },
                 {
                     id: 3,
                     name: 'Training',
                     price: 200,
-                    discount: 0,
                     added: false
                 }
-            ]
+            ],
+            discount: 0
         }
     },
     methods: {
@@ -46,11 +43,28 @@ const AppCheckout = {
     },
     computed: {
         checkoutTotal(){
-            let total = 0;
+            let total = 0,
+                selectedCount = 0,
+                minPrice = 0,
+                minPriceId = false;
             for(let i in this.servicesList){
                 if(this.servicesList[i].added){
                     total += this.servicesList[i].price
+                    selectedCount += 1;
+                    if(!minPrice){
+                        minPrice = this.servicesList[i].price
+                        minPriceId = this.servicesList[i].id
+                    }else{
+                        if(this.servicesList[i].price < minPrice){
+                            minPrice = this.servicesList[i].price
+                            minPriceId = this.servicesList[i].id
+                        }
+                    }
                 }
+            }
+            if(selectedCount > 2 && minPriceId !== false){
+                this.discount = this.servicesList[minPriceId].price / 2
+                total -= this.discount
             }
             return total
         }
