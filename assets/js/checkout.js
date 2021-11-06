8747bd895a50452dcedf786a34877a87099a35f1
Yurii <yura93lobur@gmail.com>
2021-11-06 22:19:34 +0200

    checkout promo start

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -8,24 +8,28 @@ const AppCheckout = {
                     id: 0,
                     name: 'Design',
                     price: 300,
+                    discount: 0,
                     added: false
                 },
                 {
                     id: 1,
                     name: 'Web development',
                     price: 400,
+                    discount: 0,
                     added: false
                 },
                 {
                     id: 2,
                     name: 'Integration',
                     price: 250,
+                    discount: 0,
                     added: false
                 },
                 {
                     id: 3,
                     name: 'Training',
                     price: 200,
+                    discount: 0,
                     added: false
                 }
             ]
