d36d0ce7115a26ea24b1ec0c1d8e9e3939401b5e
Yurii <yura93lobur@gmail.com>
2021-11-06 21:49:32 +0200

    simple checkout init

---
+++ assets/js/checkout.js
@@ -0,0 +1,59 @@
+const AppCheckout = {
+    data() {
+        return {
+            checkoutTitle: 'Select services',
+            checkoutSumCaption: 'Sum',
+            servicesList: [
+                {
+                    id: 0,
+                    name: 'Design',
+                    price: 300,
+                    added: false
+                },
+                {
+                    id: 1,
+                    name: 'Web development',
+                    price: 400,
+                    added: false
+                },
+                {
+                    id: 2,
+                    name: 'Integration',
+                    price: 250,
+                    added: false
+                },
+                {
+                    id: 3,
+                    name: 'Training',
+                    price: 200,
+                    added: false
+                }
+            ]
+        }
+    },
+    methods: {
+        toggleService(serviceId){
+            for(let i in this.servicesList){
+                if(this.servicesList[i].id === serviceId){
+                    this.servicesList[i].added = !this.servicesList[i].added;
+                }
+            }
+        }
+    },
+    computed: {
+        checkoutTotal(){
+            let total = 0;
+            for(let i in this.servicesList){
+                if(this.servicesList[i].added){
+                    total += this.servicesList[i].price
+                }
+            }
+            return total
+        }
+    },
+    watch: {
+
+    }
+}
+
+Vue.createApp(AppCheckout).mount('#checkout')
