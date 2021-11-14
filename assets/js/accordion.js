8bf2c085f5af2a6d35f2c635109a82defb83680e
Yurii <yura93lobur@gmail.com>
2021-11-14 23:54:22 +0200

    accordion flush

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -42,13 +42,64 @@ const AppAccordion = {
                 }
             }
         }
-    },
-    computed: {
+    }
+}
 
+const AppAccordionFlush = {
+    data() {
+        return {
+            accordionTitle: 'Accordion Flush',
+            accordionList: [
+                {
+                    id: 0,
+                    title: 'Accordion Item #1',
+                    body: '<strong>This is the first item\'s accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It\'s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.',
+                    state: 'opened'
+                },
+                {
+                    id: 1,
+                    title: 'Accordion Item #2',
+                    body: '<strong>This is the second item\'s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It\'s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.',
+                    state: 'closed'
+                },
+                {
+                    id: 2,
+                    title: 'Accordion Item #3',
+                    body: '<strong>This is the third item\'s accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It\'s also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.',
+                    state: 'closed'
+                }
+            ]
+        }
     },
-    watch: {
-
+    methods: {
+        toggleItem(itemId, event){
+            let itemContainer = event.target.closest('.accordion__item'),
+                itemBodyInner = itemContainer.querySelector('.accordion__item-body-inner'),
+                mainContainer = event.target.closest('.accordion'),
+                allItemContainers = mainContainer.querySelectorAll('.accordion__item')
+            for(let j = 0; j < allItemContainers.length; ++j){
+                allItemContainers[j].querySelector('.accordion__item-body').style.height = 0
+            }
+            if(itemBodyInner && itemBodyInner.clientHeight){
+                for(let i in this.accordionList){
+                    if(this.accordionList[i].id === itemId){
+                        if(this.accordionList[i].state === 'closed'){
+                            this.accordionList[i].state = 'opened';
+                            itemContainer.querySelector('.accordion__item-body').style.height = itemBodyInner.clientHeight + 'px'
+                        }else{
+                            this.accordionList[i].state = 'closed';
+                            itemContainer.querySelector('.accordion__item-body').style.height = 0
+                        }
+                    }else{
+                        if(this.accordionList[i].state === 'opened'){
+                            this.accordionList[i].state = 'closed';
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
 Vue.createApp(AppAccordion).mount('#accordion')
+Vue.createApp(AppAccordionFlush).mount('#accordion_flush')
